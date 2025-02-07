14eea5b9ba5a26bce2b680b8390de5b739bd552e
Santosh Kumar <santoshjnvropar@gmail.com>
2025-02-07 10:16:49 +0000

    Removed backend folder from frontend-dev branch

--- backend/middleware/log_request.py
+++
@@ -1,8 +0,0 @@
-class LogRequestMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        print(f"Host: {request.get_host()}, Path: {request.path}")
-        return self.get_response(request)
-    
